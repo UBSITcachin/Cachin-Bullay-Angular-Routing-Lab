d8058e34e9ecdc3eaa6c2f9b5e1e692c89a86422
x <x@f112.ub.lan>
2025-03-04 16:15:34 +0800

    Added and generated Route Guard

--- angular-routing-lab/src/app/app-routing.module.ts
+++ angular-routing-lab/src/app/app-routing.module.ts
@@ -11,7 +11,8 @@ const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
   { path: 'about', component: AboutComponent, children:[{path: 'profile', component: ProfileComponent}] },
-  { path: 'contact', component: ContactComponent }];
+  { path: 'contact', component: ContactComponent },
+  { path: 'admin', loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule) }];
 
 @NgModule({
   declarations: [],
