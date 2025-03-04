2931dae45206a07f1b0e7374199aa56a7d7411a5
UBSITbullay <20227234@s.ubaguio.edu>
2025-03-04 16:08:05 +0800

    added child route

--- angular-routing-lab/src/app/app-routing.module.ts
+++ angular-routing-lab/src/app/app-routing.module.ts
@@ -4,12 +4,13 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { RouterModule, Routes } from '@angular/router';
+import { ProfileComponent } from './about/profile/profile.component';
 
 
 const routes: Routes = [
   { path: '', redirectTo: '/home', pathMatch: 'full' },
   { path: 'home', component: HomeComponent },
-  { path: 'about', component: AboutComponent },
+  { path: 'about', component: AboutComponent, children:[{path: 'profile', component: ProfileComponent}] },
   { path: 'contact', component: ContactComponent }];
 
 @NgModule({
