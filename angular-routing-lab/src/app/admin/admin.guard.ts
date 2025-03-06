2036d92d9d507f42a1d27780455fcb6a6debaacc
UBSIT_cachin <20162294@s.ubaguio.edu>
2025-03-06 15:57:24 +0800

    Implemented admin guard logic

--- angular-routing-lab/src/app/admin/admin.guard.ts
+++ angular-routing-lab/src/app/admin/admin.guard.ts
@@ -1,5 +1,16 @@
-import { CanActivateFn } from '@angular/router';
+import { Injectable } from '@angular/core';
+import { CanActivate, ActivatedRouteSnapshot, RouterStateSnapshot, UrlTree } from '@angular/router';
+import { Observable } from 'rxjs';
 
-export const adminGuard: CanActivateFn = (route, state) => {
-  return true;
-};
+@Injectable({
+  providedIn: 'root'
+})
+export class AdminGuard implements CanActivate {
+  canActivate(
+    next: ActivatedRouteSnapshot,
+    state: RouterStateSnapshot
+  ): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+    
+    return true; 
+  }
+}
