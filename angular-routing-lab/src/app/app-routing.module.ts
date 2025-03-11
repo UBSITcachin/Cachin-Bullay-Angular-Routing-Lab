69e3b0a90bd4ee7605a5bc9075d74f1a89c99e9e
x <x@f112.ub.lan>
2025-03-11 15:42:45 +0800

    Adding admin guard and updated routing

--- angular-routing-lab/src/app/app-routing.module.ts
+++ angular-routing-lab/src/app/app-routing.module.ts
@@ -4,16 +4,24 @@ import { HomeComponent } from './home/home.component';
 import { AboutComponent } from './about/about.component';
 import { ContactComponent } from './contact/contact.component';
 import { ProfileComponent } from './about/profile/profile.component';
+import { AdminGuard } from './admin/admin.guard';
 
 const routes: Routes = [
-    { path: '', redirectTo: '/home', pathMatch: 'full' },
-    { path: 'home', component: HomeComponent },
-    { path: 'about', component: AboutComponent, children: [
-      { path: 'profile', component: ProfileComponent }
-    ]},
-    { path: 'contact', component: ContactComponent }
-  ];
- 
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { 
+    path: 'about', component: AboutComponent, children: [
+      { path: 'profile', component: ProfileComponent } // Fixed 'pathprofile'
+    ]
+  },
+  { path: 'contact', component: ContactComponent },
+  { 
+    path: 'admin', 
+    loadChildren: () => import('./admin/admin.module').then(m => m.AdminModule), 
+    canActivate: [AdminGuard] 
+  }
+];
+
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
   exports: [RouterModule]
